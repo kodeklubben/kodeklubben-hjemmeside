ee811fda0c4ef3d3b29e77d5ef89541f2e882505
Kristoffer Ravik Andresen <kristandre@users.noreply.github.com>
2016-11-15 12:31:11 +0100

    Update week number function (#102)

--- app/Resources/assets/js/app.js
+++ app/Resources/assets/js/app.js
@@ -98,7 +98,12 @@ function sortTableByAttribute($table, attr, ascending){
 
 Date.prototype.getWeekNumber = function(){
     var d = new Date(+this);
-    d.setHours(0,0,0);
-    d.setDate(d.getDate()+4-(d.getDay()||7));
-    return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7);
+    d.setHours(0,0,0,0);
+    // Set to nearest Thursday: current date + 4 - current day number
+    // Make Sunday's day number 7
+    d.setDate(d.getDate() + 4 - (d.getDay()||7));
+    // Get first day of year
+    var yearStart = new Date(d.getFullYear(),0,1);
+    // Calculate full weeks to nearest Thursday
+    return Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
 };
