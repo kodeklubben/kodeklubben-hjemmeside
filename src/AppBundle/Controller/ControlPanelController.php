4c4b720a85cfcbe6e8575f246ec3d6973e65b648
Kristoffer Ravik Andresen <kristandre@users.noreply.github.com>
2016-11-15 22:07:24 +0100

    Add email addresses on email page in CP (#104)

--- src/AppBundle/Controller/ControlPanelController.php
+++ src/AppBundle/Controller/ControlPanelController.php
@@ -7,30 +7,35 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 /**
  * Class ControlPanelController.
- * 
+ *
  * @Route("/kontrollpanel")
  */
 class ControlPanelController extends Controller
 {
     /**
      * @return \Symfony\Component\HttpFoundation\Response
-     * 
+     *
      * @Route("/", name="control_panel")
      */
     public function showAction()
     {
-        return $this->render('@App/control_panel/show.html.twig', array(
-            // ...
+        return $this->render('@App/control_panel/show.html.twig', array(// ...
         ));
     }
 
     /**
      * @return \Symfony\Component\HttpFoundation\Response
-     * 
+     *
      * @Route("/epost", name="cp_email")
      */
     public function showEmailAction()
     {
-        return $this->render('@App/control_panel/show_email.html.twig');
+        $currentSemester = $this->getDoctrine()->getRepository('AppBundle:Semester')->findCurrentSemester();
+        $courses = $this->getDoctrine()->getRepository('CourseBundle:Course')->findBySemester($currentSemester);
+
+        return $this->render('@App/control_panel/show_email.html.twig', array(
+            'semester' => $currentSemester,
+            'courses' => $courses,
+        ));
     }
 }
